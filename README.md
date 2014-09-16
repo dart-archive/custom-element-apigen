e2dd65474517a8bb5a209222ca6656fa1f1f7050
Sigmund Cherem <sigmund@google.com>
2014-09-16 15:37:52 -0700

    Migrate tool implementation from the core_elements package

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-custom-element-apigen
-=====================
+# Custom Elements APIGen
 
-Tool to generate Dart APIs for polymer custom elements written in JavaScript
+This package contains a tool that lets you wrap custom elements written with
+polymer.js and provide a Dart API for them.
+
+The tool assumes that the JavaScript code was packaged using bower and follows
+bower packages conventions.
+
+To use it, you need to:
+  * configure bower to put the packages under `lib/src/` instead of the default
+    `bower_components`. For exmaple, with a `.bowerrc` file as follows:
+
+      {
+        "directory": "lib/src"
+      }
+
+  * setup your `bower.json` file
+  * install packages via `bower install`
+  * (one time) create a configuration file for this tool
+  * run the tool via `pub run custom_elements_apigen:update configfile.yaml`
+
+There is not much documentation written for this tool. You can find examples of
+how this tool is used in the [core-elements][1] and [paper-elements][2]
+packages.
+
+[1]: https://github.com/dart-lang/core-elements/
+[2]: https://github.com/dart-lang/paper-elements/
---
+++ bin/update.dart
@@ -0,0 +1,39 @@
+#!/usr/bin/env dart
+// Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+// This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+// The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+// The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+// Code distributed by Google as part of the polymer project is also
+// subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:custom_element_apigen/generate_dart_api.dart' as generator;
+
+main(args) {
+  generator.main(args);
+  // TODO(sigmund): find out if we can use a bower override for this.
+  var file = new File(path.join('lib', 'src', 'polymer', 'polymer.html'));
+  if (!file.existsSync()) {
+    print('error: lib/src/polymer/polymer.html not found. This tool '
+        'requires that you first run `bower install`, and configure it '
+        'to place all sources under `lib/src/`. See README for details.');
+    exit(1);
+  }
+  file.writeAsStringSync(_POLYMER_HTML_FORWARD);
+}
+
+const String _POLYMER_HTML_FORWARD = '''
+<!--
+Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+-->
+
+<!-- Dart note: load polymer for Dart and JS from the same place -->
+<link rel="import" href="../../../../packages/polymer/polymer.html">
+${generator.EMPTY_SCRIPT_WORKAROUND_ISSUE_11}
+''';
